b66bdf6b6a44ac564f55e89304a12032cf10d35c
Karuppan3 <sanchana3344@gmail.com>
2025-06-16 10:17:09 +0530

    Initial commit

---
+++ app/src/main/kotlin/io/bashpsk/imagekropdemo/MainActivity.kt
@@ -0,0 +1,30 @@
+package io.bashpsk.imagekropdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.bashpsk.imagekropdemo.ui.theme.ImageKropTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+
+            ImageKropTheme {
+
+            }
+        }
+    }
+}
